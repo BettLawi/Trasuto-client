d3d212fdd71b005c1719f87adb6ba94bbadf763b
Lawi Bett <lawibett06@gmail.com>
2024-11-03 11:17:18 +0300

    Initial commit: React boilerplate with Vite

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// src/App.jsx
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to My React App with Vite</h1>
+    </div>
+  );
+}
+
+export default App;
