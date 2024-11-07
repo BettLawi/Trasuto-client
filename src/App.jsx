0fd61e2914d2694b8a51090e418ad2da8d55062c
Lawi Bett <lawibett06@gmail.com>
2024-11-07 13:51:04 +0300

    First commits

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-// src/App.jsx
+// src/App.js
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './components/Login';
+import Register from './components/Register';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to My React App with Vite</h1>
+    <div className="bg-whiteBackground min-h-screen flex items-center justify-center text-primaryBlue">
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
