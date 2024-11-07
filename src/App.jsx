a8d33af78f0560d3293161ce4d12c05a88b480d1
Lawi Bett <lawibett06@gmail.com>
2024-11-07 17:11:09 +0300

    Vite Config changes 2nd

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Register from './components/Register';
 import Login from './components/Login';
-import Home from './components/Home';
 
 function App() {
   return (
     <div className="bg-whiteBackground min-h-screen flex items-center justify-center text-primaryBlue">
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
         </Routes>
