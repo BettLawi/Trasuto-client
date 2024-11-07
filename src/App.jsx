4f44b35c030b2b057df26ab7663d9ce539799de2
Lawi Bett <lawibett06@gmail.com>
2024-11-07 17:45:22 +0300

    Add GitHub Actions workflow for automatic deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Register from './components/Register';
 import Login from './components/Login';
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Login />,
+    },
+    {
+      path: "/profile",
+      element: <Register />,
+    },
+  ],
+  { basename: import.meta.env.BASE_URL }
+);
 
 function App() {
   return (
     <div className="bg-whiteBackground min-h-screen flex items-center justify-center text-primaryBlue">
-      <Router>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
-      </Router>
+      <RouterProvider router={router} />
     </div>
   );
 }
